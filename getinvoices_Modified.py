534cf430c6b8ac3e9e6cfd11fcb89037b3e3a868
ShivBhati <bhatishivam0007@gmail.com>
2023-12-05 12:57:00 +0530

    final

--- getinvoices_Modified.py
+++ getinvoices_Modified.py
@@ -14,16 +14,7 @@ HEADERS = {
 
 
 
-# def fetch_field_metadata():
-#     response = requests.get(f"https://api.knack.com/v1/objects/object_1", headers=HEADERS)
-#     metadata = response.json().get("object", {}).get("fields", [])
-#     # print("Field Metadata:", json.dumps(metadata, indent=4))
-#     return metadata
 
-# def get_header_mappings():
-#     field_metadata = fetch_field_metadata()
-#     header =  {field["key"]: field.get("name", field["key"]) for field in field_metadata}
-#     return header
 
 
 def create_df(records):
@@ -32,8 +23,8 @@ def create_df(records):
                        'GL Description #1','GL Description #2','Gl Account #','Sub Account #','Bank','Status',
                        'Terms Code','Date of Order','Transaction Type','Detail Type','Currency','Currencydate',
                        'Office','Tax Code']
-    df = pd.DataFrame(columns = column)
-    df = ({'Invoice Date': records.get('field_2'),'Beneficiary Name': records.get('field_3'),'Due Date': records.get('field_4'),
+    
+    data = ({'Invoice Date': records.get('field_2'),'Beneficiary Name': records.get('field_3'),'Due Date': records.get('field_4'),
                        'Grand Total':records.get('field_5'),'Service Code': records.get('field_6'),'Client Number': records.get('field_7'),'Invoice #':records.get('field_8'),
                        'Confirmation Number':records.get('field_9'),'Line Item Number':records.get('field_10'),
                        'Cost Description':records.get('field_11'),'GL Description #1':records.get('field_12'),'GL Description #2':records.get('field_13'),
@@ -41,6 +32,7 @@ def create_df(records):
                        'Terms Code' :records.get('field_18'),'Date of Order' :records.get('field_19'),'Transaction Type' :records.get('field_20'),
                        'Detail Type' :records.get('field_21'),'Currency' :records.get('field_22'),'Currencydate' :records.get('field_23'),'Office':records.get('field_24'),
                        'Tax Code':records.get('field_25')})
+    df = pd.DataFrame([data], columns = column)
     return df
     
 
@@ -59,7 +51,7 @@ def create_Vendor_Specific_Csv(records):
 
     sorted_data = sorted(records, key=lambda x: x["field_1"])
     vendor = 'N'
-    Name = ''
+    
     column = ['Invoice Date','Beneficiary Name','Due Date','Grand Total','Service Code',
                        'Client Number','Invoice #','Confirmation Number','Line Item Number','Cost Description',
                        'GL Description #1','GL Description #2','Gl Account #','Sub Account #','Bank','Status',
@@ -72,30 +64,23 @@ def create_Vendor_Specific_Csv(records):
         if vendor == 'N':
             vendor = record.get("field_1")
         if vendor == record.get("field_1"):
-            record_data = create_df(record) 
-            print(record_data)          
-            df2 =pd.DataFrame(record_data)                      
-            df1 = df1.append(df2, ignore_index=True)
+            df2 =  create_df(record)                     
+            df1 = df1._append(df2, ignore_index =True)
         elif vendor != record.get("field_1"):
             save_records_to_csv(df1, vendor)
             df1 = pd.DataFrame()
             record_data = create_df(record)
             df2 =pd.DataFrame(record_data)            
-            df1 = df1.append(df2, ignore_index=True)
-            vendor == record.get("field_1")
+            df1 = df1._append(df2, ignore_index=True)
+            vendor = record.get("field_1")
     save_records_to_csv(df1, vendor)
-    # save_records_to_csv(rec, headers_mapping, current_datetime, vendor)
-
-
-
-
-
-# def update_records(records):
-#     ##Update the records both locally and on the Knack platform. # save_records_to_csv(records, headers_mapping, currentdatetime)
-#     for record in records:
-#         if record.get("field_49") == "N":
-#             record["field_49"] = "Y"
-#             update_record_on_knack(record["id"], {"field_49": "Y"})
+    
+def update_records(records):
+    ##Update the records both locally and on the Knack platform. 
+    for record in records:
+        if record.get("field_49") == "N":
+            record["field_49"] = "Y"
+            update_record_on_knack(record["id"], {"field_49": "Y"})
 
 def get_all_records():
     all_records = []
@@ -124,7 +109,7 @@ def get_all_records():
         all_records.extend(records)
         page_number += 1
 
-    # update_records(all_records)
+    update_records(all_records)
     
 
     return all_records
@@ -133,12 +118,19 @@ def save_records_to_csv(x,vendor):
     
     currentdatetime =  datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     filename=f"\\\\biztalk\\3EDrop\\vchr3eloadfile_{vendor}_{currentdatetime}.csv" 
-    filename = f"B:\\Python\\Git\\Knack_Voucher_detail_Extraction\\Test\\vchr3eloadfile_{vendor}_{currentdatetime}.csv"  
+    additional_filename = f"e:\\APInvoicesv2AttachCSV\\vchr3eloadfile_{vendor}_{currentdatetime}.csv"  
+    # testdrive = f"B:\\Python\\Git\\Knack_Voucher_detail_Extraction\\Test\\vchr3eloadfile_{vendor}_{currentdatetime}.csv"
+    # testdrive2 = f"B:\\Python\\Git\\Knack_Voucher_detail_Extraction\\Test2\\vchr3eloadfile_{vendor}_{currentdatetime}.csv"
    # Function to save records to a given filename
     def write_to_csv(filename):
         x.to_csv(filename, encoding='utf-8')
+        status = print(f"A file is created with the name of {filename}")
+        return status
     # Save to both file locations
     write_to_csv(filename)
+    write_to_csv(additional_filename)
+    # write_to_csv(testdrive)
+    # write_to_csv(testdrive2)
     
 
 def main():
